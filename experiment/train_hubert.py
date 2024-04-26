c1abafad4a5ac2cdc438298ec48ee22fae47c17e
Yiming Tang <tangyimi@discovery2.hpc.usc.edu>
2024-04-26 03:10:17 -0700

    hubert

--- experiment/train_hubert.py
+++ experiment/train_hubert.py
@@ -45,7 +45,7 @@ def train_hubert():
                          strategy='ddp_find_unused_parameters_true',
                          callbacks=[checkpoint_callback],
                          num_sanity_val_steps=0,
-                         precision=16)
+                         precision=hparams.precision)
     trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
 
     wandb.finish()
--- test.py
+++ test.py
@@ -1,10 +1,42 @@
 import torch, os
-from torchvision.io import read_video
-path = '/project/msoleyma_1026/CMU-MOSEI/MOSEI/Videos/Segmented/Combined'
-file1 = '_UNQDdiAbWI_1.mp4'
-file2 = '_UNQDdiAbWI_0.mp4'
+import torch.nn as nn
 
-frames, _, _ = read_video(str(os.path.join(path, file1)), output_format="TCHW")
-print(f'Frames: {frames.shape}')
-frames, _, _ = read_video(str(os.path.join(path, file2)), output_format="TCHW")
-print(f'Frames: {frames.shape}')
+from utils.dataset import get_dataloader
+from module.marlin_module import MarlinModule
+from module.hubert_module import HuBERTModule
+
+from torchmetrics import Accuracy
+
+marlin_path = '/scratch1/tangyimi/emotion/ckpt/marlin_small/best.ckpt'
+hubert_path = '/scratch1/tangyimi/emotion/ckpt/hubert/lr5e-05/best.ckpt'
+
+def late_fusion():
+    marlin_model = MarlinModule.load_from_checkpoint(marlin_path)
+    hubert_model = HuBERTModule.load_from_checkpoint(hubert_path)
+
+    marlin_model.eval()
+    hubert_model.eval()
+
+    _, val_loader, _ = get_dataloader('cremad',
+                                      batch_size=2,
+                                      fine_tune=True)
+    device = marlin_model.device
+
+    accuracy_metric = Accuracy(task="multiclass", num_classes=6).to(device)
+
+    with torch.no_grad():
+        for batch in val_loader:
+            batch = {k: v.to(device) for k, v in batch.items()}
+            
+            y, marlin_output = marlin_model(batch)
+            _, hubert_output = hubert_model(batch)
+
+            marlin_output_prob, hubert_output_prob = torch.softmax(marlin_output, dim=1), torch.softmax(hubert_output, dim=1)
+            output = marlin_output_prob + hubert_output_prob
+
+            accuracy_metric.update(output, y)
+
+    print(f'Accuracy: {accuracy_metric.compute()}')
+
+if __name__ == '__main__':
+    late_fusion()
