6fd1d13f216b5265dedb748e798c3cce6a98255e
Yiming Tang <tangyimi@discovery2.hpc.usc.edu>
2024-05-05 15:31:05 -0700

    f1 bug fixed

--- experiment/evaluate.py
+++ experiment/evaluate.py
@@ -1,11 +1,9 @@
 import torch, argparse
 from sklearn.metrics import f1_score
-from experiment.lightning_module import ExperimentModule
 from utils.dataset import get_dataloader
 from lightning import seed_everything
 from tqdm import tqdm
 
-best ='/home/tangyimi/emotion/ckpt/mosei/sharedencoder/SharedEncoder_lr=0.001/best.ckpt'
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -13,13 +11,20 @@ def get_args():
     parser.add_argument('--seed', type=int, default=0)
     parser.add_argument('--data', type=str)
     parser.add_argument('--fine_tune', action='store_true')
+    parser.add_argument('--average', type=str, default='micro')
+    parser.add_argument('--shared', action='store_true')
 
     return parser.parse_args()
 
 
-def evalute_f1(model_path, seed, data, fine_tune, average='macro'):
+def evalute_f1(model_path, seed, data, fine_tune, shared, average='micro'):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    if shared:
+        from experiment.shared_encoder_module import ExperimentModule
+    else:
+        from experiment.lightning_module import ExperimentModule
+    
     model = ExperimentModule.load_from_checkpoint(model_path)
     model = model.to(device)
     model.eval()
@@ -36,16 +41,21 @@ def evalute_f1(model_path, seed, data, fine_tune, average='macro'):
         y_pred = []
         for batch in tqdm(val_loader):
             batch = {k: v.to(device) for k, v in batch.items()}
-            y, y_hat = model(batch)
+            if shared:
+                output = model(batch)
+                y = output.label
+                y_hat = output.multimodal_logits
+            else:
+                y, y_hat = model(batch)
             y_true.extend(y.cpu().numpy())
             y_pred.extend(y_hat.argmax(dim=1).cpu().numpy())
 
     f1 = f1_score(y_true, y_pred, average=average)
 
-    print(f'Model {type(model.model).__name__} F1 score on {data}: {f1}')
+    print(f'Model {type(model.model).__name__} F1 score ({average}) on {data}: {f1}')
 
 
 if __name__ == '__main__':
     args = get_args()
-    evalute_f1(args.model_path, args.seed, args.data, args.fine_tune)
+    evalute_f1(args.model_path, args.seed, args.data, args.fine_tune, args.shared, args.average)
     
